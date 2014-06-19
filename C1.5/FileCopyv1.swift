e8ccbe317cd3a0427aa39ef8729cbc36e1c6260d
y3kready <y3kready@users.noreply.github.com>
2014-06-19 06:49:45 +0400

    Chapter 1.5.1 Example

---
+++ C1.5/FileCopyv1.swift
@@ -0,0 +1,10 @@
+#!/usr/bin/env xcrun swift -i
+//Chapter 1.5 - FileCopyv1.swift
+//Copy input characters to standard output; 1st version
+//can be run directly from command line
+
+import Foundation
+
+var stdin = NSFileHandle.fileHandleWithStandardInput()
+
+print(NSString(data: stdin.availableData, encoding: NSUTF8StringEncoding))
