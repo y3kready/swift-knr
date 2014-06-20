5069ca44642340691e3be1405c41cd19dc4280f8
y3kready <y3kready@users.noreply.github.com>
2014-06-20 07:37:31 +0400

    Chapter 1.5 Examples

--- C1.5/FileCopyv1.swift
+++ C1.5/FileCopyv1.swift
@@ -1,6 +1,6 @@
 #!/usr/bin/env xcrun swift -i
 //Chapter 1.5 - FileCopyv1.swift
-//Copy input characters to standard output; 1st version
+//Copy input characters to standard output
 //can be run directly from command line
 
 import Foundation
